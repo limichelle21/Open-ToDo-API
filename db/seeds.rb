2253c59e0a0f5f0b5a446a39280151fee88fed0d
Michelle Li <limichelle21@gmail.com>
2016-02-16 23:05:40 -0800

    added item and list serializers, updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,41 @@
 include Faker
-include RandomData
+# require "random_data"
+# include RandomData
 
 10.times do 
 	user = User.create!(
 		name: Faker::Name.name,
 		email: Faker::Internet.email,
-		password: Faker::Internet.password(10,20)
+		password_digest: Faker::Internet.password(10,20)
 		)
 end
 
 users = User.all
 
-15.times do 
-	Item.create!(
-		item: Faker::Lorem.sentence,
-		completed: RandomData.random_boolean,
+
+5.times do 
+	List.create!(
+		title: Faker::Lorem.words,
 		user: users.sample
 		)
 end
 
-items = Item.all? 
+lists = List.all
+
+
+15.times do 
+	Item.create!(
+		item: Faker::Lorem.sentence,
+		completed: true,
+		list: lists.sample
+		)
+end
+
+items = Item.all
 
 
 
 puts "Seed finished."
+puts "#{List.count} lists created"
 puts "#{Item.count} items created"
 puts "#{User.count} users created"
