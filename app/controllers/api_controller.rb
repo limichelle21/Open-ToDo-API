47ec7fb784679a026c4a4ce2a67611723f6a90c1
Michelle Li <limichelle21@gmail.com>
2016-03-03 22:26:40 +0700

    updated api controller to check for current user auth

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,12 +1,15 @@
 class ApiController < ApplicationController
 
-	skip_before_action :verify_authenticity_token
+	skip_before_action :verify_authenticity_token, :authenticate_user!
 
 private
 	def authenticated?
-		authenticate_or_request_with_http_basic {|username, password| User.where ( username: username, password: password).present? }
-
+		authenticate_or_request_with_http_basic do |email, password|
+			@current_user = User.find_by(email: email)
+			begin
+				@current_user.valid_password?(password)
+			rescue
+				render json: {success: false, message: "Authentication Failed"}, status: 401
+		end
 	end
-
-
 end
